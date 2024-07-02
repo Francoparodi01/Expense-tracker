f711bf719e49107f5f77f6d0442c0440ac7de024
francooparodi01 <francoparodi2001@gmail.com>
2024-07-02 11:41:25 -0300

    Método crud funcional

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,8 +1,9 @@
-const { addIncome } = require('../controllers/income')
+const { addIncome, getIncome, deleteIncome } = require('../controllers/income')
 
 const router = require('express').Router()
 
 router.post('/add-income', addIncome)
-
+    .get('/get-incomes', getIncome)
+    .delete('/delete-income/:id', deleteIncome)
 
 module.exports = router
