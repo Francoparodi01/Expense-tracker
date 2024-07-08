08455db318bbecb5ad7cc3325c01fa85a1959907
francooparodi01 <francoparodi2001@gmail.com>
2024-07-08 19:33:56 -0300

    modificando archivo vercel.json

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,30 @@
-const express = require('express')
-const cors = require('cors')
-const { db } = require('./db/db')
-require('dotenv').config()
-const {readdirSync} = require('fs')
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/db');
+require('dotenv').config();
+const { readdirSync } = require('fs');
+const path = require('path');
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-
-//Middlewares
+// Middlewares
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-//Rutas
-readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
- 
-const server = () =>{
-    db()
-    app.listen(PORT, () =>{
-        console.log('Escuchando el puerto:', PORT)
-    })
-}
+// Rutas
+const routesPath = path.join(__dirname, 'routes');
+readdirSync(routesPath).map((file) => {
+    const routeFilePath = path.join(routesPath, file);
+    app.use('/api/v1', require(routeFilePath));
+});
 
-server()
+const server = () => {
+    db();
+    app.listen(PORT, () => {
+        console.log('Escuchando el puerto:', PORT);
+    });
+};
+
+server();
